cce505a0e91bf0d825ff6c23df768e7d2e297a13
blue618020 <130967356+blue618020@users.noreply.github.com>
2023-08-04 18:11:08 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,6 @@
 # 노출되면 안됨!!!
 
 class Config:
-    # # DB 관련 정보
-    # HOST = 'yhdb.csigebpxoyml.ap-northeast-2.rds.amazonaws.com'
-    # DATABASE = 'posting_db'
-    # DB_USER = 'posting_db_user'
-    # DB_PASSWORD = '1234'
-
-    # # 비번 암호화
-    # SALT = '0417helloqkqn'
-
-    # # JWT 환경 변수 세팅
-    # JWT_SECRET_KEY = 'hello~!by@'
-    # JWT_ACCESS_TOKEN_EXPIRES = False # 특정 시간이 지나면 로그아웃 할 건가?=false
-    # PROPAGATE_EXCEPTIONS = True
 
     # S3 관련 변수
     S3_BUCKET = 'blue-rekognition'
