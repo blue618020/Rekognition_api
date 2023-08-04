3921ac7ce0ace57fb620942ba4189241d045bbde
blue618020 <130967356+blue618020@users.noreply.github.com>
2023-08-04 18:10:49 +0900

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,3 @@ class Config:
     S3_BUCKET = 'blue-rekognition'
     S3_BASE_URL = 'https://'+S3_BUCKET+'.s3.amazonaws.com/'
 
-    # aws 액세스 키
-    AWS_ACCESS_KEY_ID = 'AKIAY7EHY7KFUDFIS47Q'
-    AWS_SECRET_ACCESS_KEY = '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'
